4e4dcaf632472c2b63936b52c1e1a9600b69667a
Aisyiah7 <nurulaisyiah.72@gmail.com>
2017-12-20 16:21:58 +0800

    Added namespace

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/account')
+            return redirect('accounts:home')
     else:
         form = RegistrationForm()
 
@@ -31,21 +31,19 @@ def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
-@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
 
         if form.is_valid():
             form.save()
-            return redirect('/account/profile')
+            return redirect('accounts:view_profile')
 
     else:
         form = EditProfileForm(instance=request.user)
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
 
-@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
@@ -55,7 +53,7 @@ def change_password(request):
             update_session_auth_hash(request, form.user)
             return redirect('/account/profile')
         else:
-            return redirect('/account/change-password')
+            return redirect('accounts:change_password')
 
     else:
         form = PasswordChangeForm(user=request.user)
